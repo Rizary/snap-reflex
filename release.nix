8bc883c30b9c5d1caa8d460b0f9a46aa21adc469
rizy <andika.riyan@gmail.com>
2017-03-21 19:54:36 +0700

    fis submodule

--- release.nix
+++ release.nix
@@ -12,24 +12,11 @@ let
 	  frontend = import ./frontend { inherit reflex-platform; };
 	};
       };
-      
-      haskell = pkgs.haskell // {
-        packages = pkgs.haskell.packages // {
-	  "${compiler}" = pkgs.haskell.packages.${compiler}.override {
-            overrides = self: super: rec {	      
-	    };
-	  };
-	};
-      };
     };
     permittedInsecurePackages = [ "webkitgtk-2.4.11" ];
   };
 
-  pkgs = nixpkgs.overridePackages (self: super: {
-    config = super.config // super.config.override { inherit config; };
-    inherit haskellPackages;
-  });
-
+  pkgs = nixpkgs.config.override { inherit config; };
 in
   { frontend = pkgs.haskellPackages.frontend; }
 
